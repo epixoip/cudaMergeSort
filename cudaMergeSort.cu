b8cf0723595354ff02a6e81d7d471b5072297324
Jeremi M Gosney <jgosney@terahash.com>
2019-02-20 17:08:09 -0600

    Pascal is sm_61, not sm_62

--- cudaMergeSort.cu
+++ cudaMergeSort.cu
@@ -32,7 +32,7 @@
     nvcc -g -O3 -m 64 -lrt -lm -o cudaMergeSort \
         -gencode arch=compute_35,code=sm_35 \
         -gencode arch=compute_52,code=sm_52 \
-        -gencode arch=compute_62,code=sm_62 \
+        -gencode arch=compute_61,code=sm_61 \
         cudaMergeSort.cu
 */
 
